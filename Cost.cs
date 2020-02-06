ab7dd52fc26c44072524a8f5f93d01a482195ae4
Grant Crofton <grantac@hotmail.com>
2020-02-06 08:52:45 +0000

    Add project files.

---
+++ Cost.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace BudgetMaster {
+    public class Cost {
+        public string Item{ get; set; }
+        public decimal? Value { get; set; }
+        public DateTime? CreationDate { get; set; }
+        public string User { get; set; }
+    }
+}
